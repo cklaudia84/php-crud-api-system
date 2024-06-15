8a4b952614170fd7ee4b6d4146d4b87f4ff9db05
cklaudia <c.klaudia84@gmail.com>
2024-06-15 18:23:14 +0200

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dell/php-crud-api-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3c1e0a888031a06ed63e66a82e3767e720cdda61',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dell/php-crud-api-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3c1e0a888031a06ed63e66a82e3767e720cdda61',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
